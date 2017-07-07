efd0e73f59fd87a6ed61dc51a1a41fee56e14848
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-07 09:05:40 +0300

    Create function to draw board created a function that allows me to draw the board

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
 the_board = {'tl':' ','tm':' ','tr':' ',
 'ml':' ','mm':' ','mr':' ',
 'bl':' ','bm':' ','br':' ',}
+
+def define_board(board):
+    # Defining each section of the board
+    top_side = board['tl']+" | "+board['tm']+" | "+board['tr']
+    middle_side = board['ml']+" | "+board['mm']+" | "+board['mr']
+    bottom_side = board['bl']+" | "+board['bm']+" | "+board['br']
+    # Getting the length of the board so that we can  create a border
+    length = len(top_side)
+
+    # Defining the board
+    print(top_side)
+    print("-" * length)
+    print(middle_side)
+    print("-" * length)
+    print(bottom_side)
